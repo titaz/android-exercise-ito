28b9829401dbdbf7c532a90fd03185d92e5f4a33
TitasHome <rukastitas.d@gmail.com>
2022-05-02 04:16:40 +0300

    +Implimented recomendation features with new fragment and button

--- app/src/main/java/lt/ito/mycooking/di/components/AppComponent.kt
+++ app/src/main/java/lt/ito/mycooking/di/components/AppComponent.kt
@@ -8,6 +8,7 @@ import lt.ito.AppFileUtils
 import lt.ito.components.schedulers.SchedulerProvider
 import lt.ito.components.test.IdlingResourceCountable
 import lt.ito.my.RecipesFragment
+import lt.ito.my.RecommendationsFragment
 import lt.ito.mycooking.ITOApplication
 import lt.ito.mycooking.UiTestModule
 import lt.ito.mycooking.attempts.AttemptsFragment
@@ -17,6 +18,7 @@ import lt.ito.mycooking.di.modules.RepositoryModule
 import lt.ito.mycooking.launcher.LauncherActivity
 import lt.ito.mycooking.recipe.RecipeFragment
 import javax.inject.Singleton
+
 
 @Singleton
 @Component(
@@ -43,8 +45,14 @@ interface AppComponent {
 
     fun inject(app: ITOApplication)
     fun inject(recipesController: RecipesFragment)
+    fun inject(recommendationsController: RecommendationsFragment)
     fun inject(attemptsController: AttemptsFragment)
     fun inject(recipeController: RecipeFragment)
     fun inject(launcherActivity: LauncherActivity)
+
+
+
+
+
 
 }
--- app/src/main/java/lt/ito/mycooking/recipes/RecyclerViewAdapter.kt
+++ app/src/main/java/lt/ito/mycooking/recipes/RecyclerViewAdapter.kt
@@ -7,69 +7,95 @@ import androidx.recyclerview.widget.RecyclerView
 import it.ito.mycooking.R
 import kotlinx.android.synthetic.main.item_recipe.view.*
 import kotlinx.android.synthetic.main.item_recipes_sectioned.view.*
+import kotlinx.android.synthetic.main.item_simple_text.view.*
 import lt.ito.components.base.ContentItem
 import lt.ito.components.base.RecyclerViewItem
 import lt.ito.components.base.SectionItem
+import lt.ito.components.base.SimpleTextItem
 import lt.ito.models.Recipe
 import lt.ito.models.color
 
 const val VIEW_TYPE_SECTION = 1
 const val VIEW_TYPE_ITEM = 2
+const val VIEW_TYPE_SIMPLE_TEXT = 3
 
-class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>()  {
+class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 
-        var data = listOf<RecyclerViewItem>()
+    var data = listOf<RecyclerViewItem>()
 
-        override fun getItemViewType(position: Int): Int {
-            if (data[position] is SectionItem) {
-                return VIEW_TYPE_SECTION
-            }
-            return VIEW_TYPE_ITEM
+    override fun getItemViewType(position: Int): Int {
+        if (data[position] is SectionItem) {
+            return VIEW_TYPE_SECTION
         }
+        return VIEW_TYPE_ITEM
+    }
 
-        override fun getItemCount(): Int {
-            return data.size
-        }
+    override fun getItemCount(): Int {
+        return data.size
+    }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            if (viewType == VIEW_TYPE_SECTION) {
-                return SectionViewHolder(
-                    LayoutInflater.from(parent.context).inflate(R.layout.item_recipes_sectioned, parent, false)
-                )
-            }
-            return ContentViewHolder(
-                LayoutInflater.from(parent.context).inflate(R.layout.item_recipe, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == VIEW_TYPE_SIMPLE_TEXT) {
+            return SectionViewHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_simple_text, parent, false)
             )
         }
+        if (viewType == VIEW_TYPE_SECTION) {
+            return SectionViewHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_recipes_sectioned, parent, false)
+            )
+        }
+        return ContentViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_recipe, parent, false)
+        )
+    }
 
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            val item = data[position]
-            if (holder is SectionViewHolder && item is SectionItem) {
-                holder.bind(item)
-            }
-            if (holder is ContentViewHolder && item is ContentItem) {
-                holder.itemView.setOnClickListener { item.onCLickListener.onClick(position, item.recipe) }
-                holder.bind(item)
-            }
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = data[position]
+        if (holder is SimpleTextViewHolder && item is SimpleTextItem) {
+            holder.bind(item)
         }
 
-        internal inner class SectionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            fun bind(item: SectionItem) {
-                itemView?.sectionText.text = item.title
-            }
+        if (holder is SectionViewHolder && item is SectionItem) {
+            holder.bind(item)
         }
+        if (holder is ContentViewHolder && item is ContentItem) {
+            holder.itemView.setOnClickListener {
+                item.onCLickListener.onClick(
+                    position,
+                    item.recipe
+                )
+            }
+            holder.bind(item)
+        }
+    }
 
-        internal inner class ContentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            fun bind(item: ContentItem) {
-                itemView.imageView.setImageURI(item.recipe.imageUrl)
-                itemView.titleView.text = item.recipe.title
-                itemView.difficultyView.text = item.recipe.difficulty.name
-                itemView.difficultyView.setTextColor(item.recipe.difficulty.color)
-            }
+    internal inner class SimpleTextViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: SimpleTextItem) {
+            itemView.simpleText.text = item.text
         }
+    }
+
+    internal inner class SectionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: SectionItem) {
+            itemView?.sectionText.text = item.title
+        }
+    }
+
+    internal inner class ContentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: ContentItem) {
+            itemView.imageView.setImageURI(item.recipe.imageUrl)
+            itemView.titleView.text = item.recipe.title
+            itemView.difficultyView.text = item.recipe.difficulty.name
+            itemView.difficultyView.setTextColor(item.recipe.difficulty.color)
+        }
+    }
+
     fun setValues(listItems: List<RecyclerViewItem>) {
         data = listItems
         notifyDataSetChanged()
     }
-    }
+}
